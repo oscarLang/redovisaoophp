dd10b38fa7f56f829c1aa872d7b2e47db1a0f11f
Oscar Lang <oscar.lang98@gmail.com>
2019-04-13 00:14:19 +0100

    added basic gameplay

--- router/02_guess.php
+++ router/02_guess.php
@@ -6,8 +6,9 @@
 */
 $app->router->get("guess/init", function () use ($app) {
     // TODO: init game
-    $game = new Osln\Guess\Guess();
-    $_SESSION["game"] = $game;
+    $guess = new Osln\Guess\Guess();
+    $_SESSION["guess"] = $guess;
+    $_SESSION["status"] = null;
     return $app->response->redirect("guess/start");
 });
 
@@ -15,13 +16,41 @@ $app->router->get("guess/init", function () use ($app) {
 $app->router->get("guess/start", function () use ($app) {
     $title = "Play the game";
     $data = [
-        "class" => "hello-world",
+        "guess" => $_SESSION["guess"],
         "content" => "start the game",
     ];
 
     $app->page->add("guess/play", $data);
+    if(isset($_SESSION["status"])) {
+        $data["status"] = $_SESSION["status"];
+        $app->page->add("guess/status", $data);
+    }
     $app->page->add("guess/debug");
     return $app->page->render([
         "title" => $title,
     ]);
+});
+
+$app->router->post("guess/start", function () use ($app) {
+    $guess = $_SESSION["guess"];
+    if (isset($_POST["input"])) {
+        $inputNumber = $_POST["input"];
+        try {
+            $_SESSION["status"] = $guess->makeGuess($inputNumber);
+            if ($inputNumber == $guess->number) {
+                $_SESSION["inputstate"] = "hidden";
+            }
+        } catch (GuessException $e) {
+            echo "Got exception: " . get_class($e) . "<hr>";
+        }
+    }
+    return $app->response->redirect("guess/start");
+});
+
+$app->router->get("guess/cheat", function () use ($app) {
+    $data = [
+        "number" => 2
+    ];
+    $app->page->add("guess/cheat", $data);
+    return $app->response->redirect("guess/start");
 });
