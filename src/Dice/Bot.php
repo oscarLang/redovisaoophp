0e8d3ba038a1d9557f83a7c28d7c6233985de48b
Oscar Lang <oscar.lang98@gmail.com>
2019-04-25 20:05:28 +0100

    Created class Bot, subclass of player

---
+++ src/Dice/Bot.php
@@ -0,0 +1,35 @@
+<?php
+namespace Osln\Dice;
+
+/**
+ * A game of dice
+ */
+class Bot extends Player
+{
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+        var_dump($this);
+    }
+
+    public function playRound()
+    {
+        $dices = new DiceHand();
+        $faces = [];
+        while(true) {
+            $dices->roll();
+            $faces = $dices->values();
+            if (!in_array(1, $faces)) {
+                $this->roundScore += $dices->sum();
+            } else {
+                $this->roundScore = 0;
+                break;
+            }
+            if ($this->roundScore > 30) {
+                $this->stay();
+                break;
+            }
+        }
+        return $faces;
+    }
+}
